027bf8a6d2870c9ae4231cde42578d46e75f0c13
root <root@LAPTOP-3F3BGL77.>
2024-12-13 12:32:00 +0100

    2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,21 +9,32 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node_2 = malloc(sizeof(binary_tree_t));
-if (!parent || !new_node_2)
-	return (NULL);
+binary_tree_t *new_node;
 
-new_node_2->n = value;
-new_node_2->parent = parent;
-new_node_2->left = NULL;
-new_node_2->right = NULL;
+	/* Check if parent is NULL */
+	if (!parent)
+		return (NULL);
 
-if (parent->right != NULL)
-{
-new_node_2->right = parent->right;
-parent->right->parent = new_node_2;
-}
-parent->right = new_node_2;
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
 
-return (new_node_2);
+	/* Initialize the new node */
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	/* Adjust pointers if parent already has a right child */
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+
+	/* Set the new node as the right child of the parent */
+	parent->right = new_node;
+
+	return (new_node);
 }
